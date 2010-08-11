63c14a15120f1cbc390cd0dd98ece1f6192f38f8
Jerry Cao <jerry.cao@amlogic.com>
2010-08-10 18:01:33 -0700

    Amlogic mali driver

---
+++ driver/Android.mk
@@ -0,0 +1,9 @@
+LOCAL_PATH:=$(call my-dir)
+
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/egl.cfg:system/lib/egl/egl.cfg
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/libEGL_mali.so:system/lib/egl/libEGL_mali.so
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/libGLESv1_CM_mali.so:system/lib/egl/libGLESv1_CM_mali.so
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/libGLESv2_mali.so:system/lib/egl/libGLESv2_mali.so
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/libMali.so:system/lib/libMali.so
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/mali.ko:system/lib/mali.ko
+
