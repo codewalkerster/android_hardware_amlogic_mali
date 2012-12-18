063c6b42c7bed4cccd1dac71d6fd3b45d95e8925
Kasin Lee <kasin.li@amlogic.com>
2012-12-18 14:45:53 +0800

    remove useless file

--- driver/cp.sh
+++
@@ -1,5 +0,0 @@
-adb push libEGL_mali.so /system/lib/egl/
-adb push libGLESv2_mali.so /system/lib/egl/
-adb push libGLESv1_CM_mali.so /system/lib/egl/
-adb push libUMP.so /system/lib/
-adb push libMali.so /system/lib/
